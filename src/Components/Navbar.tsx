3a0585cd986f531aced1d419389a5731bd11c7e3
Chaveguapo <catakeas@gmail.com>
2023-11-24 09:57:08 +0100

    Ajusting the margin

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,12 @@
 import logo from "../assets/images/logo.svg";
+
 import "./Navbar.css";
 
 export function Navbar() {
   return (
     <header className="navbar-container">
       <img src={logo} alt={logo}></img>
+      <div className="hamburger-menu"></div>
       <ul className="navmenu">
         <li>
           <a href="#home">Home</a>
@@ -16,7 +18,7 @@ export function Navbar() {
           <a href="#popular">Popular</a>
         </li>
         <li className="navlink">
-          <a href="trending">Trending</a>
+          <a href="#trending">Trending</a>
         </li>
         <li className="navlink">
           <a href="#categories">Categories</a>
