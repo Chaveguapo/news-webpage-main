9f25160d3c918b6bd62085714e1e6056d8e27795
Chaveguapo <catakeas@gmail.com>
2023-11-27 14:54:31 +0100

    Desk version complete

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -58,20 +58,20 @@ export function Container() {
         </div>
 
         <div className="set-post">
-          <img src={reviving01}></img>
+          <img src={top02}></img>
           <div className="textbox-post">
-            <h2>01</h2>
-            <h3>Reviving Retro PC's</h3>
-            <p> What happpens when old PC's are given modern upgrades?</p>
+            <h2>02</h2>
+            <h3>Top 10 Laptops of 2022</h3>
+            <p> Our best picks for various needs and budgets.</p>
           </div>
         </div>
 
         <div className="set-post">
-          <img src={reviving01}></img>
+          <img src={growth03}></img>
           <div className="textbox-post">
-            <h2>01</h2>
-            <h3>Reviving Retro PC's</h3>
-            <p> What happpens when old PC's are given modern upgrades?</p>
+            <h2>03</h2>
+            <h3>The Growth of Gaming</h3>
+            <p> How the pandemic has sparked fresh opportunities.</p>
           </div>
         </div>
       </div>
